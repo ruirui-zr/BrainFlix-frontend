2ccec70427e64f5821637f49b3e9d250e76ddf10
Rui <ellazhang0412@gmail.com>
2024-10-23 23:35:47 -0400

    complete header section

---
+++ src/components/Avatar/Avatar.jsx
@@ -0,0 +1,10 @@
+import AvatarImg from "../../assets/images/Mohan-muruge.jpg"
+import "./Avatar.scss"
+
+export default function Avatar(){
+    return(
+        <img className="avatar" src={AvatarImg} alt="mohan_avatar" />
+    )
+}
+
+
---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import LogoIcon from "../../assets/logo/BrainFlix-logo.svg"
+
+export default function Logo() {
+  return (
+  <img className="logo" src={LogoIcon} alt="logo" />
+  )
+}
