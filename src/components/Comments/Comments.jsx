71f74676b6d3f31bd7671026a51e88956de2314d
Rui <ellazhang0412@gmail.com>
2024-10-27 12:05:30 -0400

    final version complete

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -3,7 +3,7 @@ import Avatar from "../Avatar/Avatar.jsx"
 import Form from "../Form/Form.jsx"
 
 
-export default function Comments({comments}) {
+export default function Comments({comments, formatDate}) {
     return (
     <>
     <Form comments={comments}/>
@@ -14,7 +14,7 @@ export default function Comments({comments}) {
             <div className="comment__detail">
                 <div className="comment__info">
                     <p className="comment__name">{comment.name}</p>
-                    <p className="comment__date">{comment.timestamp}</p>
+                    <p className="comment__date">{formatDate(comment.timestamp)}</p>
                 </div>
                 <p className="comment__content">{comment.comment}</p>
             </div>
